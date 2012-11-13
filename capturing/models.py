2d14fc1cb916e462584e0bc07b213d6947b0bb6b
A.Kosarevskiy <alexkos37@gmail.com>
2012-11-14 02:18:36 +0700

    save new site object #14

--- capturing/models.py
+++ capturing/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
-class Site(models.Model):
-	url  = models.CharField(max_length=100)
-	user = models.ForeignKey(User)
+class NewSites(models.Model):
+    url  = models.CharField(max_length=100)
+    user = models.ForeignKey(User)
 
 class TextSite(models.Model):
-	url   = models.CharField(max_length=100)
-	title = models.CharField(max_length=200)
-	text  = models.TextField()
-	site  = models.ForeignKey(Site)
+    url   = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    text  = models.TextField()
+    site  = models.ForeignKey(NewSites)
--- capturing/views.py
+++ capturing/views.py
@@ -1,11 +1,34 @@
+from capturing.forms import FormSite
+from capturing.models import NewSites
 from django.template import RequestContext
+from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
     context = RequestContext(request)
 
+    if request.method == 'POST':
+        if request.user.is_authenticated():
+            userid = request.user.id
+            form   = FormSite(request.POST)
+
+            if form.is_valid():
+                url  = form.cleaned_data['url']
+                user = User.objects.get(id=userid)
+                site = NewSites(url=url, user=user)
+                site.save()
+
+                return HttpResponseRedirect(request.path)
+    else:
+        form = FormSite()
+
+        return render_to_response('enter_url.html', 
+                                  {'form':form},
+                                   context_instance=context)
+
     return render_to_response('enter_url.html', 
-                               context_instance=context)
+                                   context_instance=context)
 
 def search(request):
     context = RequestContext(request)
