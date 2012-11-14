e3de9eeb8733fc7fd0f5a2385110013552d2e933
A.Kosarevskiy <alexkos37@gmail.com>
2012-11-14 15:41:21 +0700

    domain name dont repeated #15

--- capturing/models.py
+++ capturing/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
 class NewSites(models.Model):
-    url  = models.CharField(max_length=100)
+    url  = models.CharField(max_length=100, unique=True)
     user = models.ForeignKey(User)
 
     def __unicode__(self):
