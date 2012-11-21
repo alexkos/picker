950ed6d9cb5ef6d93e5659b220e3098a137e2b9c
A.Kosarevskiy <alexkos37@gmail.com>
2012-11-21 13:21:43 +0700

    display search word

--- capturing/templatetags/picktags.py
+++ capturing/templatetags/picktags.py
@@ -5,15 +5,18 @@ from django.contrib.auth.forms import AuthenticationForm
 register = Library()
 
 def parttext(object_text, search_word):
-    print '-----------------'
-    print search_word
-    print '-----------------'
-
     display_text = ''
 
     for part in object_text.text.split('.'):
-        place_numb = part.lower().find(search_word.lower())
-        display_text += part[place_numb-30:place_numb+30]
+        place_numb  = part.lower().find(search_word.lower())
+        len_word    = len(search_word)
+
+        if place_numb==0:
+            display_text += '<em>' + part[place_numb:len_word] + '</em>' + \
+                            part[len_word:place_numb+30]
+        else:
+            display_text += part[place_numb-30:place_numb+30]
+            # display_text += part[place_numb-30:place_numb+30]
 
     return display_text
 
