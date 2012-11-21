a49493fc806313ea1c990ea5cba6b8db769ef462
A.Kosarevskiy <alexkos37@gmail.com>
2012-11-21 22:00:26 +0700

    edit code

--- capturing/views.py
+++ capturing/views.py
@@ -47,19 +47,19 @@ def display_links(request):
     if request.method == 'GET':
         userid = request.user.id
         form   = FormDom(userid, auto_id=False)
-        if request.GET:
-            if request.GET['domen']:
-                siteid = request.GET['domen']
-                data   = NewSites.objects.get(id=siteid)
 
-                return render_to_response('display_links.html', 
-                                          {'form_links' : form,
-                                           'links'      : data,},
-                                           context_instance=context)
+        siteid = request.GET.get('domen','')
 
-        return render_to_response('display_links.html', 
-                                  {'form_links':form},
-                                   context_instance=context)
+        if not siteid:
+            data = ''
+        else:
+            data = NewSites.objects.get(id=siteid)
+
+    return render_to_response('display_links.html', 
+                              {'form_links' : form,
+                               'links'      : data,},
+                               context_instance=context)
+
 @login_required
 def search(request):
     context = RequestContext(request)
@@ -69,7 +69,8 @@ def search(request):
         form   = FormSearchText(userid, auto_id=False)
         match  = ''
 
-        if request.GET.get('domen','') and request.GET.get('text',''):
+        # if request.GET.get('domen','') and request.GET.get('text',''):
+        if request.GET:
             siteid = request.GET.get('domen','')
             search = request.GET.get('text','')
             form   = FormSearchText(userid, search, auto_id=False)
@@ -77,24 +78,20 @@ def search(request):
 
             pages  = data.textsite_set.extra(where=['text_tsv @@ plainto_tsquery(%s)'],
                                              params=[search])
-
             if not pages:
-                match = 'Don\'t find of match'
-            txt = []
-            for page in pages:
-                if search in page.text:
-                    txt.append(page.text)
-            print search
+                match  = 'Don\'t find of match'
+        else:
+            pages  = []
+            search = ''
+            # for page in pages:
+            #     if search in page.text:
+            #         print search
 
-            return render_to_response('search.html', 
-                                      {'form_search': form,
-                                       'pages'      : pages,
-                                       'search_word': search,
-                                       'match'      : match},
-                                       context_instance=context)
-    
         return render_to_response('search.html', 
-                                 {'form_search':form,
-                                  'match'      : match},
+                                  {'form_search': form,
+                                   'pages'      : pages,
+                                   'search_word': search,
+                                   'match'      : match},
                                    context_instance=context)
-
+    
+    return HttpResponseRedirect(reverse(main_page)) 
