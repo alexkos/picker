6e43c69e43424a9e5248e6d92a4177afb3e22079
A.Kosarevskiy <alexkos37@gmail.com>
2012-11-30 15:19:48 +0700

    added local settings

---
+++ picker/local_settings_default.py
@@ -0,0 +1,16 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+EMAIL_HOST             = ''
+EMAIL_HOST_USER        = ''
+DEFAULT_FROM_EMAIL     = ''
+EMAIL_HOST_PASSWORD    = ''
+EMAIL_PORT             = '587'
